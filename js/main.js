15cfcf4973698c98492ca4f16271e02f5b998865
kpovel <kar.pavel123@gmail.com>
2022-04-17 10:56:48 +0300

    refactored code

--- js/main.js
+++ js/main.js
@@ -1,14 +1,12 @@
-import {TEMPLATE_ELEMENT, UI_ELEMENTS, changeParamsOnNow, changeParamsOnDetails} from "./view.js";
+import {UI_ELEMENTS, changeParamsTabNow, changeParamsTabDetails} from "./view.js";
 
-const savedCity = []
+const savedCities = [];
 const SERVER_URL = 'https://api.openweathermap.org/data/2.5/weather';
 const API_KEY = '4783b73cfe02019303d03a9d793cc64b';
 
 
 UI_ELEMENTS.INPUT.addEventListener('keydown', function (e) {
-    if (e.key === 'Enter') {
-        getWeather()
-    }
+    if (e.key === 'Enter') getWeather()
 })
 UI_ELEMENTS.BUTTON.addEventListener('click', getWeather)
 
@@ -18,82 +16,82 @@ function getWeather(switchOfCity) {
 
     fetch(url)
         .catch(() => {
-            throw new URIError('error url')
+            throw new URIError('error url');
         })
         .then(response => response.json())
         .then(item => {
-            changeParamsOnNow(item)
-            changeParamsOnDetails(item)
+            changeParamsTabNow(item);
+            changeParamsTabDetails(item);
         })
         .then(() => {
-            const cityInSaved = savedCity.findIndex(item => item === UI_ELEMENTS.WEATHER_CITY_NOW.textContent)
+            const cityInSaved = savedCities.findIndex(item => item === UI_ELEMENTS.NOW.CITY.textContent);
             if (~cityInSaved) {
-                UI_ELEMENTS.WEATHER_NOW_BUTTON.style.background = 'url("./img/heart_red.svg")'
+                UI_ELEMENTS.NOW.BUTTON.style.background = 'url("./img/heart_red.svg")';
             } else {
-                UI_ELEMENTS.WEATHER_NOW_BUTTON.style.background = 'url("./img/heart.svg")'
+                UI_ELEMENTS.NOW.BUTTON.style.background = 'url("./img/heart.svg")';
             }
         })
         .catch((err) => {
             if (err.name === 'URIError') {
-                alert(err)
+                alert(err);
             } else if (err.name === 'TypeError') {
-                alert(`${err.name}: undefined city`)
+                alert(`${err.name}: undefined city`);
+            }else {
+                alert(err);
             }
         })
         .finally(() => UI_ELEMENTS.INPUT.value = null)
 }
 
 
-UI_ELEMENTS.WEATHER_NOW_BUTTON.addEventListener('click', saveCity)
+UI_ELEMENTS.NOW.BUTTON.addEventListener('click', manipulationSavedCities)
 
-function saveCity() {
-    const cityNow = UI_ELEMENTS.WEATHER_CITY_NOW.textContent
-    const templateCity = TEMPLATE_ELEMENT.CITY_ITEM.content.cloneNode(true)
-    const includeCity = savedCity.findIndex(item => item === cityNow)
-    const isNotEmptySavedList = UI_ELEMENTS.WEATHER_NOW_BUTTON.style.background === 'url("./img/heart.svg")'
+function manipulationSavedCities() {
+    const cityNow = UI_ELEMENTS.NOW.CITY.textContent;
+    const templateCity = UI_ELEMENTS.TEMPLATE_ELEMENT.CITY_ITEM.content.cloneNode(true);
+    const includeCity = savedCities.findIndex(item => item === cityNow);
+    const isNotEmptySavedList = UI_ELEMENTS.NOW.BUTTON.style.background === 'url("./img/heart.svg")';
 
     if (~includeCity) {
-        savedCity.splice(includeCity, 1)
-        const cityList = document.querySelectorAll('.city-list__item')
-        cityList[includeCity].remove()
+        savedCities.splice(includeCity, 1);
+        const cityList = document.querySelectorAll('.city-list__item');
+        cityList[includeCity].remove();
 
-        UI_ELEMENTS.WEATHER_NOW_BUTTON.style.background = 'url("./img/heart.svg")'
+        UI_ELEMENTS.NOW.BUTTON.style.background = 'url("./img/heart.svg")';
     } else {
-        savedCity.push(cityNow)
+        savedCities.push(cityNow);
 
-        templateCity.firstElementChild.firstElementChild.textContent = cityNow
-        UI_ELEMENTS.CITY_LIST.append(templateCity)
+        templateCity.firstElementChild.firstElementChild.textContent = cityNow;
+        UI_ELEMENTS.CITY_LIST.append(templateCity);
 
-        UI_ELEMENTS.WEATHER_NOW_BUTTON.style.background = 'url("./img/heart_red.svg")'
+        UI_ELEMENTS.NOW.BUTTON.style.background = 'url("./img/heart_red.svg")';
     }
 
-    if (isNotEmptySavedList) {
-        chooseSavedCity()
-    }
+    if (isNotEmptySavedList) chooseSavedCity();
 
-    const cityList = document.querySelectorAll('.city-list__close-btn')
-    for (let city of cityList) {
-        city.addEventListener('click', deleteCityByButtonClose)
+    const closeButtonList = document.querySelectorAll('.city-list__close-btn');
+    for (const button of closeButtonList) {
+        button.addEventListener('click', deleteCityByButtonClose);
     }
-    console.log(savedCity)
+    console.log(savedCities);
 }
 
 function deleteCityByButtonClose() {
-    const thisCity = this.previousElementSibling.textContent
-    const indexCity = savedCity.findIndex(item => item === thisCity)
+    const thisCity = this.previousElementSibling.textContent;
+    const indexCity = savedCities.findIndex(item => item === thisCity);
 
-    savedCity.splice(indexCity, 1)
-    this.parentElement.remove()
-    if (UI_ELEMENTS.WEATHER_CITY_NOW.textContent === thisCity) {
-        UI_ELEMENTS.WEATHER_NOW_BUTTON.style.background = 'url("./img/heart.svg")'
+    savedCities.splice(indexCity, 1);
+    this.parentElement.remove();
+    if (UI_ELEMENTS.NOW.CITY.textContent === thisCity) {
+        UI_ELEMENTS.NOW.BUTTON.style.background = 'url("./img/heart.svg")';
     }
 }
 
 function chooseSavedCity() {
-    const cityList = document.querySelectorAll('.city')
+    const cityList = document.querySelectorAll('.city');
 
     cityList[cityList.length - 1].addEventListener('click', function () {
-        const searchCity = this.textContent
-        getWeather(searchCity)
+        const searchCity = this.textContent;
+        getWeather(searchCity);
     })
 }
--- js/view.js
+++ js/view.js
@@ -1,69 +1,76 @@
 export const UI_ELEMENTS = {
     INPUT: document.querySelector('.search__input'),
     BUTTON: document.querySelector('.search__btn'),
-    WEATHER_NOW: document.querySelector('.weather-now__temperature'),
-    WEATHER_CITY_NOW: document.querySelector('.title-city-now'),
-    WEATHER_NOW_BUTTON: document.querySelector('.weather-now__btn'),
-    WEATHER_NOW_IMG: document.querySelector('.weather-now__img'),
+
     CITY_LIST: document.querySelector('.city-list'),
     CITY_LIST_ITEM: document.querySelectorAll('.city-list__item'),
     CITY_LIST_CLOSE: document.querySelectorAll('.city-list__close-btn'),
-    WEATHER_DETAILS_TITLE: document.querySelector('.weather-details__title'),
-    WEATHER_DETAILS_TEMPERATURE: document.querySelector('.temperature'),
-    WEATHER_DETAILS__FEELS_LIKE: document.querySelector('.feels_like'),
-    WEATHER_DETAILS_WEATHER: document.querySelector('.weather'),
-    WEATHER_DETAILS_SUNRISE: document.querySelector('.sunrise'),
-    WEATHER_DETAILS_SUNSET: document.querySelector('.sunset'),
-}
-export const TEMPLATE_ELEMENT = {
-    CITY_ITEM: document.getElementById('city-item')
-}
 
+    NOW: {
+        TEMPERATURE: document.querySelector('.weather-now__temperature'),
+        CITY: document.querySelector('.title-city-now'),
+        BUTTON: document.querySelector('.weather-now__btn'),
+        IMG: document.querySelector('.weather-now__img'),
+    },
+
+    DETAILS: {
+        TITLE: document.querySelector('.weather-details__title'),
+        TEMPERATURE: document.querySelector('.temperature'),
+        FEELS_LIKE: document.querySelector('.feels_like'),
+        WEATHER: document.querySelector('.weather'),
+        SUNRISE: document.querySelector('.sunrise'),
+        SUNSET: document.querySelector('.sunset'),
+    },
+
+    TEMPLATE_ELEMENT: {
+        CITY_ITEM: document.getElementById('city-item'),
+    },
+}
 
 const tabs = document.querySelectorAll('.main-tabs__item')
 for (let tab of tabs) {
-    tab.addEventListener('click', switchTab)
+    tab.addEventListener('click', switchTab);
 }
 
 function switchTab() {
-    const navTab = document.querySelector('.main-tabs__item--active')
-    let activeTab = document.querySelector('.main-tabs__block--active')
-    navTab.classList.remove('main-tabs__item--active')
-    activeTab.classList.remove('main-tabs__block--active')
+    const navTab = document.querySelector('.main-tabs__item--active');
+    const activeTab = document.querySelector('.main-tabs__block--active');
+    navTab.classList.remove('main-tabs__item--active');
+    activeTab.classList.remove('main-tabs__block--active');
 
-    this.classList.add('main-tabs__item--active')
-    const idClickTab = this.getAttribute('href')
-    const idMainTabs = document.querySelector(idClickTab)
-    idMainTabs.classList.add('main-tabs__block--active')
+    this.classList.add('main-tabs__item--active');
+    const idClickTab = this.getAttribute('href');
+    const idMainTabs = document.querySelector(idClickTab);
+    idMainTabs.classList.add('main-tabs__block--active');
 }
 
 
-export function changeParamsOnNow(item) {
-    const tempCelsius = Math.round(item.main.temp - 273.15)
-    UI_ELEMENTS.WEATHER_NOW.textContent = `${tempCelsius}°`
-    UI_ELEMENTS.WEATHER_CITY_NOW.textContent = `${item.name}`
+export function changeParamsTabNow(item) {
+    const tempCelsius = Math.round(item.main.temp - 273.15);
+    UI_ELEMENTS.NOW.TEMPERATURE.textContent = `${tempCelsius}°`;
+    UI_ELEMENTS.NOW.CITY.textContent = `${item.name}`;
 
-    const icon = item.weather[0].icon
-    UI_ELEMENTS.WEATHER_NOW_IMG.src = `https://openweathermap.org/img/wn/${icon}@4x.png`
+    const icon = item.weather[0].icon;
+    UI_ELEMENTS.NOW.IMG.src = `https://openweathermap.org/img/wn/${icon}@4x.png`;
 }
 
-export function changeParamsOnDetails(item) {
-    UI_ELEMENTS.WEATHER_DETAILS_TITLE.textContent = `${item.name}`
-    UI_ELEMENTS.WEATHER_DETAILS_TEMPERATURE.textContent = `${Math.round(item.main.temp - 273.15)}°`
-    UI_ELEMENTS.WEATHER_DETAILS__FEELS_LIKE.textContent = `${Math.round(item.main.feels_like - 273.15)}°`
-    UI_ELEMENTS.WEATHER_DETAILS_WEATHER.textContent = `${item.weather[0].main}`
-    UI_ELEMENTS.WEATHER_DETAILS_SUNRISE.textContent = function () {
-        const hours = new Date(item.sys.sunrise * 1000).getHours()
-        const minutes = new Date(item.sys.sunrise * 1000).getMinutes()
-        const replaceHours = hours < 10 ? `0${hours}` : hours
-        const replaceMinutes = minutes < 10 ? `0${minutes}` : minutes
-        return `${replaceHours}:${replaceMinutes}`
-    }()
-    UI_ELEMENTS.WEATHER_DETAILS_SUNSET.textContent = function () {
-        const hours = new Date(item.sys.sunset * 1000).getHours()
-        const minutes = new Date(item.sys.sunset * 1000).getMinutes()
-        const replaceHours = hours < 10 ? `0${hours}` : hours
-        const replaceMinutes = minutes < 10 ? `0${minutes}` : minutes
-        return `${replaceHours}:${replaceMinutes}`
-    }()
+export function changeParamsTabDetails(item) {
+    UI_ELEMENTS.DETAILS.TITLE.textContent = `${item.name}`;
+    UI_ELEMENTS.DETAILS.TEMPERATURE.textContent = `${Math.round(item.main.temp - 273.15)}°`;
+    UI_ELEMENTS.DETAILS.FEELS_LIKE.textContent = `${Math.round(item.main.feels_like - 273.15)}°`;
+    UI_ELEMENTS.DETAILS.WEATHER.textContent = `${item.weather[0].main}`;
+    UI_ELEMENTS.DETAILS.SUNRISE.textContent = function () {
+        const hours = new Date(item.sys.sunrise * 1000).getHours();
+        const minutes = new Date(item.sys.sunrise * 1000).getMinutes();
+        const replaceHours = hours < 10 ? `0${hours}` : hours;
+        const replaceMinutes = minutes < 10 ? `0${minutes}` : minutes;
+        return `${replaceHours}:${replaceMinutes}`;
+    }();
+    UI_ELEMENTS.DETAILS.SUNSET.textContent = function () {
+        const hours = new Date(item.sys.sunset * 1000).getHours();
+        const minutes = new Date(item.sys.sunset * 1000).getMinutes();
+        const replaceHours = hours < 10 ? `0${hours}` : hours;
+        const replaceMinutes = minutes < 10 ? `0${minutes}` : minutes;
+        return `${replaceHours}:${replaceMinutes}`;
+    }();
 }
